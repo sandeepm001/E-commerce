2528bcfc5862504bae8c5cd036db1c0115a74b4e
sandeep muthyala <sandeepm9392@gmail.com>
2026-05-24 10:52:46 +0530

    added the account section and wishhlist,cart improvement

--- ecommerce/src/Components/Popular/Popular.jsx
+++ ecommerce/src/Components/Popular/Popular.jsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from 'react'
 import React from 'react'
-import data_product from '../Assets/Frontend_Assets/data'
 import Item from '../Item/Item'
 import './Popular.css'
 const Popular = () => {
+
+  const [popularinmen,setPopularinmen] = useState([]);
+
+  useEffect(()=>{
+      fetch('http://localhost:4000/popularinmen')
+      .then((response) => response.json())
+      .then((data)=>setPopularinmen(data))
+    },[])
   return (
     <div className='popular'>
-        <h1>Popular In Women</h1>
+        <h1>Popular In Men</h1>
         <hr />
         <div className="popular-items">
-            {data_product.map((item,i)=>{
+            {popularinmen.map((item,i)=>{
                 return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
             })}
         </div>
